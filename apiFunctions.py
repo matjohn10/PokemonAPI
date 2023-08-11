3deebf53fd9616ec5dc719a3333e4be2dd123c5b
Mathieu Johnson <117942546+matjohn10@users.noreply.github.com>
2023-08-11 15:16:33 +0900

    Update apiFunctions.py

--- apiFunctions.py
+++ apiFunctions.py
@@ -26,7 +26,7 @@ TYPES = ['Normal',
 'Steel',
 'Fairy']
 
-conn = create_engine(url=f'mysql+pymysql://{os.getenv("DEV_USER")}:{os.getenv("DEV_PWD")}@221.152.117.194:3306/{os.getenv("DB_NAME")}')
+conn = create_engine(url=f'mysql+pymysql://{os.getenv("AWS_USERNAME")}:{os.getenv("AWS_PASSWORD")}@{os.getenv("AWS_ENDPOINT")}:3306/{os.getenv("AWS_DB_NAME")}')
 select_pokemons = 'SELECT * FROM pokemons'
 select_types = 'SELECT * FROM types'
 select_images = 'SELECT * FROM poke_images'
