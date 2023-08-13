1f9d3f9845c557c451676914c27c4ab40730bbd7
Mathieu Johnson <117942546+matjohn10@users.noreply.github.com>
2023-08-13 23:25:17 +0900

    Update image.js

--- static/image.js
+++ static/image.js
@@ -7,11 +7,11 @@ const backBtn = document.querySelector(".svg-arrow-img");
 const handleClick = (e) => {
   e.preventDefault();
   const newId = Math.floor(Math.random() * (1008 - 1) + 1);
-  const newURL = `${DEV_DOMAIN}pokemon-images/${newId}/show`;
+  const newURL = `${DEPLOYED_DOMAIN}pokemon-images/${newId}/show`;
   window.location.replace(newURL);
 };
 
 randomBtn.addEventListener("click", handleClick);
 backBtn.addEventListener("click", () => {
-  window.location.replace(DEV_DOMAIN);
+  window.location.replace(DEPLOYED_DOMAIN);
 });
